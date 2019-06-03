5476d5f71dec871ea8a79b1b38e9d4fdaa2ea422
wangyh2016 <850138226@qq.com>
2019-06-03 10:54:00 +0800

    Update setup.sh

--- scripts/front_end/trias-oauth/setup.sh
+++ scripts/front_end/trias-oauth/setup.sh
@@ -3,7 +3,6 @@ set -e
 echo '1.start mysql....'
 service mysql start
 sleep 3
-echo `service mysql status`
 echo 'create database and write data'
 mysql < /mysql/create_write.sql
 echo '2.create and write done....'
