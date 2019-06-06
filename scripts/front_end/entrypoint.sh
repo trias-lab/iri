c22f07d70167422708d08727a2dff45b6155ba52
wangyh2016 <850138226@qq.com>
2019-06-06 14:20:10 +0800

    [fix #91] add go npm and nodejs

--- scripts/front_end/entrypoint.sh
+++ scripts/front_end/entrypoint.sh
@@ -1,2 +1,3 @@
 java -jar /opt/trias/oauth/client/oauth-resource-1.0-SNAPSHOT.jar &
-java -jar /opt/trias/oauth/server/oauth-server-1.0-SNAPSHOT.jar
+java -jar /opt/trias/oauth/server/oauth-server-1.0-SNAPSHOT.jar  &
+go run main.go 
