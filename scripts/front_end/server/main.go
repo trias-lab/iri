d4b856226b72eb482bb37c41c54bad6c21cbf957
jdyuankai <yuank1987@163.com>
2019-07-19 16:20:22 +0800

    [fix #138] modify iota server address as a dynamic param for go client

--- scripts/front_end/server/main.go
+++ scripts/front_end/server/main.go
@@ -3,11 +3,28 @@ package main
 import (
 	v "./vue"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 )
 
+var (
+	host string
+)
+
+func init() {
+	flag.StringVar(&host, "host", "", "Iota server host, http://127.0.0.1:14700")
+}
+
 func main() {
+	flag.Parse()
+	if host == "" {
+		fmt.Fprintln(os.Stderr, "Usage: go run main.go -host [-file] \nOption:")
+		flag.PrintDefaults()
+		return
+	}
+
 	http.HandleFunc("/AddNode", AddNode)
 	http.HandleFunc("/QueryNodes", QueryNodes)
 	http.HandleFunc("/QueryNodeDetail", QueryNodeDetail)
@@ -30,7 +47,7 @@ func AddNode(writer http.ResponseWriter, request *http.Request) {
 		fmt.Println(err)
 		request.Body.Close()
 	}
-
+	addNodeRequest.Url = host
 	var o v.OCli
 	response := o.AddAttestationInfoFunction(addNodeRequest)
 
@@ -50,7 +67,7 @@ func QueryNodes(writer http.ResponseWriter, request *http.Request) {
 		fmt.Println(err)
 		request.Body.Close()
 	}
-
+	queryNodesRequest.Url = host
 	var o v.OCli
 	response := o.GetRankFunction(queryNodesRequest)
 
@@ -70,7 +87,7 @@ func QueryNodeDetail(writer http.ResponseWriter, request *http.Request) {
 		fmt.Println(err)
 		request.Body.Close()
 	}
-
+	detailRequest.RequestUrl = host
 	var o v.OCli
 	response := o.QueryNodeDetail(detailRequest)
 	if err := json.NewEncoder(writer).Encode(response); err != nil {
