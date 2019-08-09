2b1f445ab28f88768b9212c5103228d05a630592
jdyuankai <yuank1987@163.com>
2019-08-09 14:26:42 +0800

    [fix #149] bug fix

--- scripts/iota_api/auth.py
+++ scripts/iota_api/auth.py
@@ -1,4 +1,4 @@
-from base64 import encodestring, decodestring
+from base64 import decodestring
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
@@ -12,7 +12,5 @@ def verify_sign(message, signature, public_key, passphrase=None):
     public_key = decodestring(public_key)
     public_key = RSA.importKey(public_key, passphrase)
     public_key = PKCS1_v1_5.new(public_key)
-    try:
-        return public_key.verify(message, signature) > 0
-    except:
-        return False
+    return public_key.verify(message, signature) > 0
+
