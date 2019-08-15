62b93de3207a90804181dd4fcfc6517995bb8c4a
jdyuankai <yuank1987@163.com>
2019-08-15 18:13:07 +0800

    [fix #149] fix ci grammar

--- scripts/front_end/server/auth/rsa_util.go
+++ scripts/front_end/server/auth/rsa_util.go
@@ -11,42 +11,44 @@ import (
 	"io/ioutil"
 )
 
+/** rsa util */
 type RSAUtil struct {
 }
 
-
-func main(){
+/** make sure package name be changed to "main" when run this method */
+func main() {
 	address := "123456"
-	sig,_ := Sign([]byte(address), crypto.SHA256)
-	base64_sig := base64.StdEncoding.EncodeToString(sig)
-	fmt.Println("sign is : ", base64_sig)
+	sig, _ := Sign([]byte(address), crypto.SHA256)
+	base64Sig := base64.StdEncoding.EncodeToString(sig)
+	fmt.Println("sign is : ", base64Sig)
 
-	sig2,_ :=  base64.StdEncoding.DecodeString(base64_sig)
+	sig2, _ := base64.StdEncoding.DecodeString(base64Sig)
 	var r RSAUtil
-	err := r.Verify([]byte(address),sig2, crypto.SHA256, "public_key.pem")
+	err := r.Verify([]byte(address), sig2, crypto.SHA256, "public_key.pem")
 	fmt.Print(err)
 }
 
+/** used to sign */
 func Sign(src []byte, hash crypto.Hash) ([]byte, error) {
 	h := hash.New()
 	h.Write(src)
 	hashed := h.Sum(nil)
-	private_key := loadSk("private.pem")
-	return rsa.SignPKCS1v15(rand.Reader, private_key, hash, hashed)
+	privateKey := loadSk("private.pem")
+	return rsa.SignPKCS1v15(rand.Reader, privateKey, hash, hashed)
 }
 
+/** export method used to verify signature */
 func (rsautil *RSAUtil) Verify(src []byte, sign []byte, hash crypto.Hash, pkPath string) error {
 	h := hash.New()
 	h.Write(src)
 	hashed := h.Sum(nil)
-	public_key:= loadPk(pkPath)
-	return rsa.VerifyPKCS1v15(public_key, hash, hashed, sign)
+	publicKey := loadPk(pkPath)
+	return rsa.VerifyPKCS1v15(publicKey, hash, hashed, sign)
 }
 
-
 func loadSk(path string) *rsa.PrivateKey {
-	privateKey,_ :=  ioutil.ReadFile(path)
-	blockPriv,_ := pem.Decode([]byte(privateKey))
+	privateKey, _ := ioutil.ReadFile(path)
+	blockPriv, _ := pem.Decode([]byte(privateKey))
 	if blockPriv == nil {
 		fmt.Println("load private key error.")
 		return nil
@@ -60,7 +62,7 @@ func loadSk(path string) *rsa.PrivateKey {
 }
 
 func loadPk(file string) *rsa.PublicKey {
-	publicKey,_ :=  ioutil.ReadFile(file)
+	publicKey, _ := ioutil.ReadFile(file)
 	blockPub, _ := pem.Decode([]byte(publicKey))
 	if blockPub == nil {
 		fmt.Println("load public key error.")
