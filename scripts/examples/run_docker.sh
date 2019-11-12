79ed969b079257508aea5b9f3b14ddadcf29696d
wangyh2016 <850138226@qq.com>
2019-11-12 14:56:01 +0800

    Modify the readme file (#190)

--- scripts/examples/run_docker.sh
+++ scripts/examples/run_docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 SCRIPTPATH=$(cd "$(dirname "$0")"; pwd)
-NAME=iota-node
+NAME=stream-iota
 TAG=v0.1-streamnet
 
-rm -rf data
-rm -rf conf
+sudo rm -rf data
+sudo rm -rf conf
 mkdir data
 mkdir conf
 touch conf/neighbors
@@ -13,7 +13,7 @@ cd ../../
 docker build -t ${NAME}:${TAG} .
 sudo docker run -d -p 14700:14700 -p 13700:13700 --name ${NAME} -v ${SCRIPTPATH}/data:/iri/data -v ${SCRIPTPATH}/conf/neighbors:/iri/conf/neighbors ${NAME}:${TAG} /entrypoint.sh
 
-CLINAME=iota-cli
-cd scripts/
-docker build -t ${CLINAME}:${TAG} .
-sudo docker run -d -p 5000:5000 -e "ENABLE_BATCHING=$1" -e "HOST_IP=$SSH_CONNECTION" --name ${CLINAME} ${CLINAME}:${TAG} /docker-entrypoint.sh
+CLINAME=stream-go
+cd scripts/front_end
+docker build -t ${CLINAME}:${TAG} -f  go_docker/Dockerfile  .
+sudo  docker run -itd --net=host -e "HOST_IP=127.0.0.1" --name  ${CLINAME}  ${CLINAME}:${TAG}
