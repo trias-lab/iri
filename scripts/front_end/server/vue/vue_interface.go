f5820d5f5475d1c6a3891ed173751fe92d970737
kari <yuank1987@163.com>
2020-02-20 16:54:28 +0800

    [fix #207] fix comment for godoc

--- scripts/front_end/server/vue/vue_interface.go
+++ scripts/front_end/server/vue/vue_interface.go
@@ -1,13 +1,12 @@
 // Copyright by StreamNet team
 // 拼装对noderank访问的参数，
 // 该类是为了适配main.go 和 noderank.go 而开发的。无特殊逻辑
-
 package vue
 
 import (
 	"fmt"
-	nr "github.com/triasteam/noderank"
 	"github.com/caryxiao/go-zlog"
+	nr "github.com/triasteam/noderank"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -15,51 +14,60 @@ import (
 	"time"
 )
 
+// Message ...
 type Message struct {
-	Code    int64 `json:"code"`
-	Timestamp int64 `json:"timestamp"`
-	Message string `json:"message"`
-	Data    interface{} `json:"data"`
+	Code      int64       `json:"code"`
+	Timestamp int64       `json:"timestamp"`
+	Message   string      `json:"message"`
+	Data      interface{} `json:"data"`
 }
+
+// DataTee ...
 type DataTee struct {
 	DataScore interface{} `json:"dataScore"`
 	DataCtx   interface{} `json:"dataCtx"`
 }
 
+// AddNodeRequest ...
 type AddNodeRequest struct {
 	Attester string `json:"attester,omitempty"`
 	Attestee string `json:"attestee,omitempty"`
-	Score    int `json:"score,omitempty"`
+	Score    int    `json:"score,omitempty"`
 	Time     string `json:"time,omitempty"`
-	Nonce    int  `json:"nonce,omitempty"`
+	Nonce    int    `json:"nonce,omitempty"`
 	Address  string `json:"address,omitempty"`
 	AuthSign string `json:"authSign,omitempty"`
 	Sign     string `json:"sign,omitempty"`
 	Host     string `json:"host,omitempty"`
 }
 
+// QueryNodesRequest ...
 type QueryNodesRequest struct {
-	Period  int64  `json:"period"`
-	NumRank int64  `json:"numRank"`
-	Url     string `json:"url,omitempty"`
-	Address string `json:"address,omitempty"`
-	AuthSign     string `json:"authSign,omitempty"`
+	Period   int64  `json:"period"`
+	NumRank  int64  `json:"numRank"`
+	Url      string `json:"url,omitempty"`
+	Address  string `json:"address,omitempty"`
+	AuthSign string `json:"authSign,omitempty"`
 }
 
+// NodeDetailRequest ...
 type NodeDetailRequest struct {
 	RequestUrl    string `json:"requestUrl,omitempty"`
 	RequestData   string `json:"requestData,omitempty"`
 	RequestMethod string `json:"requestMethod,omitempty"`
 }
 
+// OCli ...
 type OCli struct {
 }
 
+// AddAtInfo ...
 type AddAtInfo interface {
 	AddAttestationInfoFunction(_data []byte) Message
 	GetRankFunction(_data []byte) Message
 }
 
+// AddAttestationInfoFunction 新增证实数据包装类，负责组装参数
 func (o *OCli) AddAttestationInfoFunction(request *AddNodeRequest) Message {
 	mess := Message{}
 	newReq := new(AddNodeRequest)
@@ -78,28 +86,30 @@ func (o *OCli) AddAttestationInfoFunction(request *AddNodeRequest) Message {
 	info[4] = strconv.Itoa(newReq.Nonce)
 	info[5] = newReq.Time
 	info[6] = newReq.Sign
-	zlog.Logger.Info("vue info split content is ",info)
+	zlog.Logger.Info("vue info split content is ", info)
 	err1 := nr.AddAttestationInfo("", request.Host, info)
 	if err1 != nil {
-		mess = Message{Code: 1, Timestamp:time.Now().Unix(), Message: "Failed to add node"}
+		mess = Message{Code: 1, Timestamp: time.Now().Unix(), Message: "Failed to add node"}
 		return mess
 	}
-	mess = Message{Code: 0, Timestamp:time.Now().Unix(), Message: "Node added successfully"}
+	mess = Message{Code: 0, Timestamp: time.Now().Unix(), Message: "Node added successfully"}
 	return mess
 }
 
+// GetRankFunction 排名查询包装类
 func (o *OCli) GetRankFunction(request *QueryNodesRequest) Message {
 	mess := Message{}
 	teescore, teectx, err1 := nr.GetRank(request.Url, request.Period, request.NumRank)
 	if teectx == nil || err1 != nil || teescore == nil {
-		mess = Message{Code: 1,Timestamp:time.Now().Unix(), Message: "Failed to query node data"}
+		mess = Message{Code: 1, Timestamp: time.Now().Unix(), Message: "Failed to query node data"}
 		return mess
 	}
 	data := DataTee{teescore, teectx}
-	mess = Message{Code: 0, Timestamp:time.Now().Unix(), Message: "Query node data successfully", Data: data}
+	mess = Message{Code: 0, Timestamp: time.Now().Unix(), Message: "Query node data successfully", Data: data}
 	return mess
 }
 
+// QueryNodeDetail ...
 func (o *OCli) QueryNodeDetail(request *NodeDetailRequest) Message {
 	if request.RequestUrl == "" {
 		return Message{Code: 0, Message: "RequestUrl is empty"}
